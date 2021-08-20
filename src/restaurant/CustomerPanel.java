14995a39d515b1668256179f553c6a8f0d83a0b7
smafsani <68096636+smafsani@users.noreply.github.com>
2021-08-20 23:26:25 +0600

    Working on customer panel.

--- src/restaurant/CustomerPanel.java
+++ src/restaurant/CustomerPanel.java
@@ -17,9 +17,19 @@ import afsani.gradiant.panels.mainboard.MainBoardBG;
 import javax.swing.JLabel;
 
 public class CustomerPanel extends JFrame{
-	
-	public CustomerPanel() {
-		setLayout(null);
+
+	public int x = 150, y = 40;
+	public button foodOrder, addProduct, addUser, addEmployee, products, usersLabel, ordersBtn, salaries;
+	public Color col1 = new Color(0, 127, 224);
+	public Color col2 = new Color(0, 70, 123);
+	public JPanel containerPanel;
+	private JLabel lblNewLabel;
+	private String globalUsername, globalPassword;
+	private button profile;
+	public CustomerPanel(String s1, String s2) {
+		globalUsername = s1;
+		globalPassword = s2;
+		getContentPane().setLayout(null);
 		setSize(910,650);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,15 +37,17 @@ public class CustomerPanel extends JFrame{
 		initialize();
 	}
 	
+	
 	private void initialize() {
 		MainBoardBG panel = new MainBoardBG();
 		panel.setSize(910, 650);
 		panel.setVisible(true);
 		panel.setLayout(null);
-		add(panel);
+		getContentPane().add(panel);
 		
 		
-		button bton = new button("Log out", new Color(31, 111, 212), new Color(129, 161, 202));
+		button bton = new button("Log out", new Color(0, 130, 220), new Color(0, 43, 90));
+		bton.setBorder(new MatteBorder(1,1,1,1, Color.red));
 		bton.setHorizontalAlignment(SwingConstants.CENTER);
 		bton.setForeground(Color.WHITE);
 		bton.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -44,23 +56,134 @@ public class CustomerPanel extends JFrame{
 		bton.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				setVisible(false);
-				Main man = new Main();
+				new Main().setVisible(true);
 			}
 			
 			public void mousePressed(MouseEvent e) {
-			bton.hover(new Color(31, 96, 178), new Color(108, 134, 168));}
+				bton.hover(new Color(0, 40, 88), new Color(0, 40, 88));}
 			
 			public void mouseReleased(MouseEvent e) {
-				bton.hover(new Color(31, 111, 212), new Color(129, 161, 202));}
+				bton.hover(new Color(0, 130, 220), new Color(0, 43, 90));}
 		});
 		panel.add(bton);
+		
+		MainBoardBG barPanel = new MainBoardBG();
+		barPanel.setBounds(5, 55, 158, 520);
+		barPanel.setColors(new Color(0, 143, 255), new Color(0, 65, 115));
+		barPanel.setBorder(new MatteBorder(2, 2, 2, 2, new Color(255, 255, 255)));
+		panel.add(barPanel);
+		barPanel.setLayout(null);
+		
+		foodOrder = new button("Food Order", col1, col2);
+		foodOrder.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				foodOrder.hover(new Color(1, 57, 99), new Color(1, 57, 99));
+				foodOrder.setSize(x+10, y);
+				setButtons("fo");
+				
+				clearContainerPanel();
+			}
+			
+		});
+		foodOrder.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
+		foodOrder.setForeground(Color.WHITE);
+		foodOrder.setFont(new Font("SansSerif", Font.BOLD, 16));
+		foodOrder.setHorizontalAlignment(SwingConstants.CENTER);
+		foodOrder.setBounds(4, 15, 150, 40);
+		foodOrder.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		barPanel.add(foodOrder);
+		
+		
+		products = new button("Products", col1, col2);
+		products.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
+		products.setHorizontalAlignment(SwingConstants.CENTER);
+		products.setForeground(Color.WHITE);
+		products.setFont(new Font("SansSerif", Font.BOLD, 16));
+		products.setBounds(4, 80, 150, 40);
+		products.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		products.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				products.hover(new Color(1, 57, 99), new Color(1, 57, 99));
+				products.setSize(x+10, y);
+				setButtons("pr");
+				
+				JPanel pan = new productsCustPanel().returnPanel();
+				pan.setBounds(10, 10, 690, 500);
+				clearContainerPanel();
+				containerPanel.add(pan);
+				
+			}
+			
+		});
+		barPanel.add(products);
+		
+		profile = new button("Profile", col1, col2);
+		profile.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
+		profile.setHorizontalAlignment(SwingConstants.CENTER);
+		profile.setForeground(Color.WHITE);
+		profile.setFont(new Font("SansSerif", Font.BOLD, 16));
+		profile.setBounds(4, 145, 150, 40);
+		profile.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		profile.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				profile.hover(new Color(1, 57, 99), new Color(1, 57, 99));
+				profile.setSize(x+10, y);
+				setButtons("pro");
+				
+				clearContainerPanel();
+			}
+			
+		});
+		barPanel.add(profile);
+
+		containerPanel = new JPanel();
+		containerPanel.setBackground(new Color(1, 57, 99));
+		containerPanel.setBounds(163, 55, 710, 520);
+		containerPanel.setVisible(true);
+		containerPanel.setLayout(null);
+		panel.add(containerPanel);
+		
+		lblNewLabel = new JLabel("Food Area");
+		lblNewLabel.setBorder(null);
+		lblNewLabel.setForeground(new Color(25, 25, 112));
+		lblNewLabel.setBackground(new Color(255, 255, 255));
+		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel.setFont(new Font("Showcard Gothic", Font.PLAIN, 35));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(0, 0, 897, 53);
+		panel.add(lblNewLabel);
+	}
+	public void setButtons(String s) {
+		if(!s.equals("fo")) {
+			foodOrder.setSize(x, y);
+			foodOrder.hover(col1, col2);
+		}
+		if(!s.equals("pr")) {
+			products.setSize(x, y);
+			products.hover(col1, col2);
+		}
+		if(!s.equals("pro")) {
+			profile.setSize(x, y);
+			profile.hover(col1, col2);
+		}
+		
+	}
+	
+	public void clearContainerPanel()
+	{
+		containerPanel.removeAll();
+		containerPanel.revalidate();
+		containerPanel.repaint();
 	}
 	public static void main(String args[])
 	{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					new CustomerPanel().setVisible(true);
+					new CustomerPanel("", "").setVisible(true);
 				}catch (Exception e) {
 				e.printStackTrace();}
 				
---
+++ src/restaurant/productsCustPanel.java
@@ -0,0 +1,237 @@
+package restaurant;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.MatteBorder;
+
+import java.awt.Color;
+import java.awt.Cursor;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+
+public class productsCustPanel {
+
+	private JFrame frame;
+	private int w = 690;
+	private int h = 550;
+	private JPanel panel, pan;
+	private JPanel searchField;
+	private Connection con = null;
+	private PreparedStatement pst = null;
+	private ResultSet rs = null;
+	private JTextField searchtextField;
+	private JTable table;
+	private DefaultTableModel model;
+	private JScrollPane scrollPane;
+	private boolean flag = false;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					productsCustPanel window = new productsCustPanel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public productsCustPanel() {
+		con = dbconnection.connectDB();
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public JPanel returnPanel() {
+		return panel;
+	}
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 706, 589);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 0, w, h);
+		panel.setLayout(null);
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel = new JLabel("Products");
+		lblNewLabel.setFont(new Font("Serif", Font.BOLD, 23));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setOpaque(true);
+		lblNewLabel.setBackground(new Color(102, 102, 204));
+		lblNewLabel.setBounds(1, 1, 688, 46);
+		panel.add(lblNewLabel);
+		
+		searchField = new JPanel();
+		searchField.setBorder(new MatteBorder(0, 0, 1, 0, (Color) Color.LIGHT_GRAY));
+		searchField.setBackground(new Color(255, 255, 255));
+		searchField.setBounds(0, 46, 690, 35);
+		panel.add(searchField);
+		searchField.setLayout(null);
+		
+		searchtextField = new JTextField();
+		searchtextField.setBounds(5, 5, 217, 25);
+		searchtextField.setForeground(Color.BLACK);
+		if(!searchtextField.isFocusOwner() && searchtextField.getText().equals("")){
+			setPlaceholder();
+		}
+		searchtextField.addFocusListener(new FocusAdapter() {
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(searchtextField.getText().equals("Enter Product Name  ")) {
+					searchtextField.setText("");
+					searchtextField.setForeground(Color.BLACK);
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(searchtextField.getText().equals("")) {
+					setPlaceholder();
+				}
+			}
+		});
+		searchField.add(searchtextField);
+		searchtextField.setColumns(10);
+		
+		JButton searchBtn = new JButton("Search");
+		searchBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(!searchtextField.getText().equals("") && !searchtextField.getText().equals("Enter Product Name  ")) {
+					model.setRowCount(0);
+					setTable(searchtextField.getText());
+					if(model.getRowCount()==0) {
+						JOptionPane.showMessageDialog(null, "No product found of Product-ID "+searchtextField.getText());
+					}
+				}
+			}
+		});
+		searchBtn.setBounds(225, 6, 75, 23);
+		searchBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		searchBtn.setBackground(new Color(102, 102, 153));
+		searchBtn.setForeground(Color.WHITE);
+		searchBtn.setFocusPainted(false);
+		searchBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		searchBtn.setFont(new Font("SansSerif", Font.BOLD, 14));
+		searchBtn.setBorder(new MatteBorder(1,1,1,1, new Color(102, 51, 255)));
+		searchField.add(searchBtn);
+		
+		JButton allBtn = new JButton("Search All");
+		allBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(flag) {
+					model.setRowCount(0);
+					setTable();
+					setPlaceholder();
+				}
+			}
+		});
+		allBtn.setBounds(610, 6, 75, 23);
+		allBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		allBtn.setBackground(new Color(102, 102, 153));
+		allBtn.setForeground(Color.WHITE);
+		allBtn.setFocusPainted(false);
+		allBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		allBtn.setFont(new Font("SansSerif", Font.BOLD, 14));
+		allBtn.setBorder(new MatteBorder(1,1,1,1, new Color(102, 51, 255)));
+		searchField.add(allBtn);
+
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(5, 81, 680, 380);
+		scrollPane.getViewport().setBackground(Color.WHITE);
+		panel.add(scrollPane);
+		
+		
+		model = new DefaultTableModel() {
+			boolean[] columnEditable = new boolean[] {
+					false, false, false, false
+			};
+			public boolean isCellEditable(int row, int col) {
+				return columnEditable[col];
+			}
+		};
+		
+		table = new JTable(model);
+		table.setRowHeight(25);
+		table.getTableHeader().setBackground(Color.BLACK);
+		table.getTableHeader().setForeground(Color.WHITE);
+		table.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 12));
+		table.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		table.setOpaque(false);
+		model.addColumn("Name");
+		model.addColumn("Category");
+		model.addColumn("Selling Price");
+		model.addColumn("Available");
+		scrollPane.setViewportView(table);
+		setTable();
+				
+		
+	}
+	private void setPlaceholder() {
+		searchtextField.setText("Enter Product Name  ");
+		searchtextField.setForeground(Color.GRAY);
+	}
+
+	private void setTable() {
+		try {
+			pst = con.prepareStatement("Select * FROM products");
+			rs = pst.executeQuery();
+			flag = false;
+			while(rs.next())
+			{
+				model.addRow(new Object[] {rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(7)});
+			}
+		} catch (Exception e) {
+		JOptionPane.showMessageDialog(null, e);}
+	}
+	private void setTable(String str) {
+		try {
+			pst = con.prepareStatement("Select * FROM products WHERE product_name=?");
+			pst.setString(1, str);
+			rs = pst.executeQuery();
+			flag = true;
+			while(rs.next())
+			{
+				model.addRow(new Object[] {rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(7)});
+			}
+		} catch (Exception e) {
+		JOptionPane.showMessageDialog(null, e);}
+	}
+
+	public void clearPanel() {
+		panel.remove(pan);
+		panel.revalidate();
+		panel.repaint();
+	}
+}
